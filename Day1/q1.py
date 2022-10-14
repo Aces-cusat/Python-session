c2100016752f364546e01e1ea398e637c7c5b681
Najid Navas <104789565+naaji-navas@users.noreply.github.com>
2022-10-14 13:35:47 +0530

    Update q1.py

--- Day1/q1.py
+++ Day1/q1.py
@@ -1,1 +1,36 @@
 print("this is my first question anwser")
+
+# Question 1
+
+name = "Naajiii"
+print("hello "+name+" Would you like to learn some Python today?")
+
+
+# Question 2
+
+name = "Naajiii"
+
+
+print("Name in Upper Case :"+name.upper())
+print("Name in Lower Case"+name.lower())
+print("Name in Title case"+name.title())
+
+
+# Question 3
+
+quoute = "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
+print(quoute)
+
+# Question 4
+name = "      Naajiii     "
+
+print(name)
+# lstrip
+print(name.lstrip())
+
+# rstrip
+print(name.rstrip())
+
+# strip
+print(name.strip())
+
