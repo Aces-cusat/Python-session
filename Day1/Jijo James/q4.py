e1f192c7d04ece9783f6938f3456e96ab5ecdd57
JijoJames04 <110347193+JijoJames04@users.noreply.github.com>
2022-10-17 12:11:40 +0530

    Create q4.py

---
+++ Day1/Jijo James/q4.py
@@ -0,0 +1,7 @@
+name = "    Jijo James  "
+
+print("Name after operating lstrip() function : ",name.lstrip())
+
+print("Name after operating rstrip() function : ",name.rstrip())
+
+print("Name after operating strip() function : ",name.strip())
